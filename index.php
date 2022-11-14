8037c8d4e83102668cf1aa065db8402e5d0740ff
Ryan Morley <ryankate2001@gmail.com>
2022-11-14 00:46:49 -0600

    sign up page adds person to the db!

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "petsitting";
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$street = $_POST["street"];
+$city = $_POST["city"];
+$usState = $_POST["state"];
+$zip = $_POST["zip"];
+$personType = $_POST["type"];
+
+try {
+  // connect to petsitting db
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+  // echo $type;
+
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // prepare an sql query
+  $q = $conn->prepare("INSERT INTO PERSON (email, phone, personFName, personLName, streetAddress, city, USState, zipCode, personType)
+  VALUES (:email, :phone, :fname, :lname, :street, :usState, :city, :zip, :personType)");
+
+  // replace the placeholders with the info from the sign up form
+  $q->bindParam(':email',$email);
+  $q->bindParam(':phone',$phone);
+  $q->bindParam(':fname', $fname);
+  $q->bindParam(':lname',$lname);
+  $q->bindParam(':street',$street);
+  $q->bindParam(':usState',$usState);
+  $q->bindParam(':city',$city);
+  $q->bindParam(':zip',$zip);
+  $q->bindParam(':personType',$personType);
+  
+  // do the sql query
+  $q->execute();
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
--- signup.php
+++ signup.php
@@ -24,28 +24,29 @@
         <h1 style="text-align:center; margin-bottom:5%;"> Pet Sitting 2.0 Sign Up!</h1>
     </div>
     <div style="margin-right:30%; margin-left:30%;">
+    <form action="index.php" method="post">
         <div class="input-group mb-3">
             <span class="input-group-text">First and Last name</span>
-            <input type="text" placeholder="First Name" aria-label="First name" class="form-control">
-            <input type="text" placeholder="Last Name" aria-label="Last name" class="form-control">
+            <input type="text" placeholder="First Name" aria-label="First name" class="form-control" name="fname">
+            <input type="text" placeholder="Last Name" aria-label="Last name" class="form-control" name="lname">
         </div>
 
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Email Address</span>
-            <input type="text" class="form-control" placeholder="Email" aria-label="E-mail_Address@example.com" aria-describedby="basic-addon1">
+            <input type="text" class="form-control" placeholder="Email" aria-label="E-mail_Address@example.com" aria-describedby="basic-addon1" name="email">
         </div>
 
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Phone Number</span>
-            <input type="text" class="form-control" placeholder="123-456-7890" aria-label="Phone" aria-describedby="basic-addon1">
+            <input type="text" class="form-control" placeholder="123-456-7890" aria-label="Phone" aria-describedby="basic-addon1" name="phone">
         </div>
 
         <div class="input-group mb-3">
             <span class="input-group-text">Address</span>
-            <input type="text" placeholder="1234 Street Road apt 567" aria-label="Address" class="form-control">
-            <input type="text" placeholder="City" aria-label="City" class="form-control">
-            <input type="text" placeholder="State" aria-label="State" class="form-control">
-            <input type="text" placeholder="Zip Code" aria-label="Zip" class="form-control">
+            <input type="text" placeholder="1234 Street Road apt 567" aria-label="Address" class="form-control" name="street">
+            <input type="text" placeholder="City" aria-label="City" class="form-control" name="city">
+            <input type="text" placeholder="State" aria-label="State" class="form-control" name="state">
+            <input type="text" placeholder="Zip Code" aria-label="Zip" class="form-control" name="zip">
         </div>
 
         <div class="input-group mb-3">
@@ -55,26 +56,27 @@
 
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Password</span>
-            <input type="text" class="form-control" placeholder="Password" aria-label="PW" aria-describedby="basic-addon1">
+            <input type="password" class="form-control" placeholder="Password" aria-label="PW" aria-describedby="basic-addon1">
         </div>
 
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Confirm Password</span>
-            <input type="text" class="form-control" placeholder="Confirm Password" aria-label="Confirm" aria-describedby="basic-addon1">
+            <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm" aria-describedby="basic-addon1">
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+            <input class="form-check-input" type="radio" name="type" id="exampleRadios1" value="0" checked>
             <label class="form-check-label" for="exampleRadios1">
                 I am a Pet Owner
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <input class="form-check-input" type="radio" name="type" id="exampleRadios2" value="1">
             <label class="form-check-label" for="exampleRadios2">
                 I am a Pet Sitter
             </label>
         </div>
+        <center><button type="submit" class="btn btn-outline-primary">Sign Up</button></a></center>
+    </form>
     </div>
-    <center><a href="index.php"><button type="button" class="btn btn-outline-primary">Sign Up</button></a></center>
   </body>
 </html>
