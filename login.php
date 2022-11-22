3d222424295ce39c6c4a825b393183d9613628d5
Ryan Morley <ryankate2001@gmail.com>
2022-11-21 20:04:54 -0600

    login sends email to index.php & sets newUser to F

--- login.php
+++ login.php
@@ -17,6 +17,9 @@
     <title>Pet Sitting 2.0 | Login</title>
   </head>
   <body>
+  <?php
+      session_start();
+  ?>
     <div style="position:relative; margin-top:1%; margin-left:95%;">
         <a href="landing.php">
             <button type="button" class="btn btn-outline-primary">Back</button>
@@ -26,16 +29,23 @@
         <h1 style="text-align:center; margin-bottom:5%;"> Pet Sitting 2.0 Login!</h1>
     </div>
     <div style="margin-right:30%; margin-left:30%;">
+    <form action="index.php" method="post">
         <div class="input-group mb-3" >
             <span class="input-group-text" id="basic-addon1">Username</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="email">
         </div>
 
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Password</span>
             <input type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1">
         </div>
+      
     </div>
-      <center><a href="index.php"><button type="button" class="btn btn-outline-primary">Log In</button></a></center>
+      <center><a href="index.php"><button type="Submit" class="btn btn-outline-primary">Log In</button></a></center>
+</form>
+<?php 
+    // note that we don't need to add to database when we get to index.php
+    $_SESSION[ 'newUser' ] = FALSE;
+?>
   </body>
 </html>
