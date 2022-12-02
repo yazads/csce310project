4b26ce1f6816c90b160ab6ec6a872b941c5944a9
Ryan Morley <ryankate2001@gmail.com>
2022-12-02 15:38:08 -0600

    made navbar reusable

--- editpet.php
+++ editpet.php
@@ -118,25 +118,7 @@ if ( window.history.replaceState ) {
     </head>
     <body style="background-color:#FAE8E0">
         <div>
-        <nav class="navbar navbar-dark bg-dark">
-            <div class="container-fluid" >
-                <a class="navbar-brand" href="index.php" style="font-size:32px; color:#FAE8E0;">
-                    <img src="assets/DogHouse.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top logo">
-                    <strong>Pet Stop</strong>
-                </a>
-                <span class="dropdown" style="padding-right:1%;">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <?php echo $personFName; ?>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end" style="margin-right:10%;">
-                    <li><a class="dropdown-item" href="index.php">Home</a></li>
-                    <li><a class="dropdown-item" href="acctinfo.php">Account Info</a></li>
-                    <li><a class="dropdown-item" href="#">Something else</a></li>
-                    <li><a class="dropdown-item" href="login.php">Sign Out</a></li>
-                    </ul>
-                </span>
-            </div>
-        </nav>
+        <?php require 'assets/navbar.php' ?>
         <div style="margin-top:1%; margin-left:95%;">
             <a href="acctinfo.php"><button type="button" class="btn btn-outline-primary">Back</button></a>
         </div>    
