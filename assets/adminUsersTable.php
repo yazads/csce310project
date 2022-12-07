e00956b59296c1cbe320294c598371f14a850d2a
Yazad <yazad_sidhwa@tamu.edu>
2022-12-07 15:46:08 -0600

    temp changes

--- assets/adminUsersTable.php
+++ assets/adminUsersTable.php
@@ -1,30 +1,30 @@
 <?php
     echo "<table style='border: solid 1px black;'>";
-    echo "<tr><th>User First Name</th><th>User Last Name</th><th>User Email</th><th>User Phone #</th> </tr>";
+    echo "<tr> <th>User ID</th> <th>User First Name</th> <th>User Last Name</th> <th>User Email</th> </tr>";
 
     class UsersTableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
-    }
+        function __construct($it) {
+            parent::__construct($it, self::LEAVES_ONLY);
+        }
 
-    function current() {
-        return "<td style='width:170px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
+        function current() {
+            return "<td style='width:170px;border:1px solid black;'>" . parent::current(). "</td>";
+        }
 
-    function beginChildren() {
-        echo "<tr>";
-    }
+        function beginChildren() {
+            echo "<tr>";
+        }
 
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
+        function endChildren() {
+            echo "</tr>" . "\n";
+        }
     }
 
     try {
     // check if user is an admin
     if($personType == 3){
         // prepare sql query to get all user information
-        $q = $conn->prepare("SELECT personFName, personLName, email, phone FROM Person");
+        $q = $conn->prepare("SELECT personID, personFName, personLName, email FROM Person");
     }
     
     // No placeholder values to bind
